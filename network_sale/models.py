cee4890f4c5605917de25c1b0dbb2f118e38b10b
anv <anv@woori.ru>
2023-10-03 19:23:50 +0300

    add models

--- network_sale/models.py
+++ network_sale/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+
+class TradingNetwork(models.Model):
+    """ Trading Network """
+    title = models.CharField(max_length=200, verbose_name='Title')
+
+    class Meta:
+        verbose_name = "Trading Network"
+        verbose_name_plural = "Trading Networks"
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    """ Products """
+    name = models.CharField(verbose_name='Product title', max_length=100)
+    model = models.CharField(verbose_name='Product model', max_length=100)
+    date = models.DateField(verbose_name='Date of going on sale', auto_now=False, auto_now_add=False)
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.name
+
+
+class Company(models.Model):
+    """ Companies """
+    TYPES = [
+        ('Factory', 'Factory'),
+        ('Retail', 'Retail'),
+        ('Individual', 'Individual')
+    ]
+    type = models.CharField(verbose_name='Type of company', choices=TYPES)
+    network = models.ForeignKey(TradingNetwork, verbose_name="Trading Network", on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='Company title', max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    country = models.CharField(verbose_name='Country', max_length=100)
+    town = models.CharField(verbose_name='Town title', max_length=100)
+    street = models.CharField(verbose_name='Street title', max_length=100)
+    structure_number = models.CharField(verbose_name='number of structure', max_length=100)
+    products = models.ManyToManyField(Product, related_name='companies')
+    created_date = models.DateField(auto_now_add=True)
+    provider = models.ForeignKey('self', on_delete=models.SET_NULL, verbose_name='Provider', related_name='Provider', null=True, blank=True)
+    arrears = models.DecimalField(max_digits=100, decimal_places=2, verbose_name='Debt', blank=True, default=0)
+
+    class Meta:
+        verbose_name = "Company"
+        verbose_name_plural = "Companies"
+
+    def __str__(self):
+        return self.name
--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Users(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name="user Email")
+    chat_id = models.IntegerField(unique=True, blank=True, null=True, default=None)
+    telegram_user_name = models.CharField(max_length=100, null=False, blank=False, unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.email}"
+
+    class Meta:
+        verbose_name = "users"
+        verbose_name_plural = 'users'
