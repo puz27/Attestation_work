1ce7fc51a805ed908ef8ff4af49f79ae0b82db54
anv <anv@woori.ru>
2023-10-04 18:11:12 +0300

    add product / provider serialazer

--- network_sale/urls.py
+++ network_sale/urls.py
@@ -1,17 +1,31 @@
 from django.urls import path
 from network_sale.views import TradingNetworkListView, TradingNetworkDetailView, TradingNetworkCreateView,\
-    TradingNetworkUpdateView, TradingNetworkDeleteView
+    TradingNetworkUpdateView, TradingNetworkDeleteView, ProductListView, ProductDetailView, ProductCreateView,\
+    ProductUpdateView, ProductDeleteView, ProviderListView, ProviderDetailView, ProviderUpdateView,\
+    ProviderDeleteView, ProviderCreateView
 
 
 app_name = "network_sale"
 
 urlpatterns = [
-    # lessons
+    # TradingNetwork
     path("trading-network/", TradingNetworkListView.as_view(), name="show_all_trading_networks"),
     path("trading-network/<int:pk>/", TradingNetworkDetailView.as_view(), name="trading_networks_show"),
     path("trading-network/create/", TradingNetworkCreateView.as_view(), name="trading_networks_create"),
     path("trading-network/update/<int:pk>/", TradingNetworkUpdateView.as_view(), name="trading_networks_update"),
     path("trading-network/delete/<int:pk>/", TradingNetworkDeleteView.as_view(), name="trading_networks_delete"),
-    ]
 
-    # payment
+    # Product
+    path("product/", ProductListView.as_view(), name="show_all_products"),
+    path("product/<int:pk>/", ProductDetailView.as_view(), name="product_show"),
+    path("product/create/", ProductCreateView.as_view(), name="product_create"),
+    path("product/update/<int:pk>/", ProductUpdateView.as_view(), name="product_update"),
+    path("product/delete/<int:pk>/", ProductDeleteView.as_view(), name="product_delete"),
+
+    # Provider
+    path("provider/", ProviderListView.as_view(), name="show_all_providers"),
+    path("provider/<int:pk>/", ProviderDetailView.as_view(), name="provider_show"),
+    path("provider/create/", ProviderCreateView.as_view(), name="provider_create"),
+    path("provider/update/<int:pk>/", ProviderUpdateView.as_view(), name="provider_update"),
+    path("provider/delete/<int:pk>/", ProviderDeleteView.as_view(), name="provider_delete"),
+    ]
--- network_sale/views.py
+++ network_sale/views.py
@@ -7,19 +7,19 @@ from network_sale.permissions import IsOwner
 
 # TradingNetwork
 class TradingNetworkListView(generics.ListAPIView):
-    """ TradingNetworkSerializer """
+    """ All Trading Network view """
     queryset = TradingNetwork.objects.all()
     serializer_class = TradingNetworkSerializer
 
 
 class TradingNetworkDetailView(generics.RetrieveAPIView):
-    """ Detailed information about course """
+    """ Detailed information about Trading Network """
     queryset = TradingNetwork.objects.all()
     serializer_class = TradingNetworkSerializer
 
 
 class TradingNetworkCreateView(generics.CreateAPIView):
-    """ Create course with owner information"""
+    """ Create Trading Network """
     queryset = TradingNetwork.objects.all()
     serializer_class = TradingNetworkSerializer
 
@@ -30,15 +30,85 @@ class TradingNetworkCreateView(generics.CreateAPIView):
 
 
 class TradingNetworkUpdateView(generics.UpdateAPIView):
-    """ Update information in course """
+    """ Update information about Trading Network """
     queryset = TradingNetwork.objects.all()
     serializer_class = TradingNetworkSerializer
 
 
 class TradingNetworkDeleteView(generics.DestroyAPIView):
-    """ Delete course """
+    """ Delete Trading Network """
     queryset = TradingNetwork.objects.all()
     serializer_class = TradingNetworkSerializer
 
 
+# Product
+class ProductListView(generics.ListAPIView):
+    """ All Product view"""
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailView(generics.RetrieveAPIView):
+    """ Detailed information about Product """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductCreateView(generics.CreateAPIView):
+    """ Create Product """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        new_course = serializer.save()
+        new_course.owner = self.request.user
+        new_course.save()
+
+
+class ProductUpdateView(generics.UpdateAPIView):
+    """ Update information about Product """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDeleteView(generics.DestroyAPIView):
+    """ Delete Product """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+# Provider
+class ProviderListView(generics.ListAPIView):
+    """ All Providers view"""
+    queryset = Provider.objects.all()
+    serializer_class = ProviderSerializer
+
+
+class ProviderDetailView(generics.RetrieveAPIView):
+    """ Detailed information about Provider """
+    queryset = Provider.objects.all()
+    serializer_class = ProviderSerializer
+
+
+class ProviderCreateView(generics.CreateAPIView):
+    """ Create Provider """
+    queryset = Provider.objects.all()
+    serializer_class = ProviderSerializer
+
+    def perform_create(self, serializer):
+        new_course = serializer.save()
+        new_course.owner = self.request.user
+        new_course.save()
+
+
+class ProviderUpdateView(generics.UpdateAPIView):
+    """ Update information about Provider """
+    queryset = Provider.objects.all()
+    serializer_class = ProviderSerializer
+
+
+class ProviderDeleteView(generics.DestroyAPIView):
+    """ Delete Provider """
+    queryset = Provider.objects.all()
+    serializer_class = ProviderSerializer
 
