0354d50402cbce3035d08f6e99d56ca52c22f736
anv <anv@woori.ru>
2023-10-04 19:32:57 +0300

    add clear money

--- network_sale/admin.py
+++ network_sale/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from network_sale.models import Provider
+from django.urls import reverse
 
-# Register your models here.
+
+@admin.register(Provider)
+class ProviderAdmin(admin.ModelAdmin):
+    """ Provider admin panel """
+    list_display = ('name', 'provider', 'town', 'arrears', 'provider')
+    list_filter = ('town',)
+    readonly_fields = ('created_date',)
+    actions = ['clear_arrears']
+
+    @admin.action(description='Clear arrears')
+    def clear_arrears(self, request, queryset):
+        queryset.update(arrears=0)
+
+
--- network_sale/models.py
+++ network_sale/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class TradingNetwork(models.Model):
     """ Trading Network """
-    title = models.CharField(max_length=200, verbose_name='Title')
+    title = models.CharField(max_length=200, verbose_name='Title of Trading network')
 
     class Meta:
         verbose_name = "Trading Network"
@@ -24,7 +24,7 @@ class Product(models.Model):
         verbose_name_plural = "Products"
 
     def __str__(self):
-        return self.name
+        return self.title
 
 
 class Provider(models.Model):
@@ -34,18 +34,20 @@ class Provider(models.Model):
         ('Retail', 'Retail'),
         ('Individual', 'Individual')
     ]
-    type = models.CharField(verbose_name='Type of company', choices=TYPES)
-    network = models.ForeignKey(TradingNetwork, verbose_name="Trading Network", on_delete=models.CASCADE)
-    name = models.CharField(verbose_name='Company title', max_length=100, unique=True)
+
+    level = models.IntegerField(verbose_name='level in hierarchy', default=0)
+    type = models.CharField(verbose_name='type of provider', choices=TYPES)
+    name = models.CharField(verbose_name='provider title', max_length=100, unique=True)
     email = models.EmailField(max_length=100, unique=True)
-    country = models.CharField(verbose_name='Country', max_length=100)
-    town = models.CharField(verbose_name='Town title', max_length=100)
-    street = models.CharField(verbose_name='Street title', max_length=100)
+    country = models.CharField(verbose_name='country', max_length=100)
+    town = models.CharField(verbose_name='town title', max_length=100)
+    street = models.CharField(verbose_name='street title', max_length=100)
     structure_number = models.CharField(verbose_name='number of structure', max_length=100)
-    products = models.ManyToManyField(Product, related_name='companies')
     created_date = models.DateField(auto_now_add=True)
-    provider = models.ForeignKey('self', on_delete=models.SET_NULL, verbose_name='Provider', related_name='Provider', null=True, blank=True)
-    arrears = models.DecimalField(max_digits=100, decimal_places=2, verbose_name='Debt', blank=True, default=0)
+    arrears = models.DecimalField(max_digits=100, decimal_places=2, verbose_name='arrears', blank=True, default=0)
+    trading_network = models.ForeignKey(TradingNetwork, verbose_name="trading Network", on_delete=models.CASCADE)
+    provider = models.ForeignKey('self', on_delete=models.SET_NULL, verbose_name='provider', related_name='types_company', null=True, blank=True)
+    products = models.ManyToManyField(Product, related_name='products')
 
     class Meta:
         verbose_name = "Provider"
